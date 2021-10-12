c631f567e996ecd29fadbf4c4e03222575121477
DAHLIACHOI <48826098+DAHLIACHOI@users.noreply.github.com>
2021-10-12 20:04:23 +0900

    counting word

---
+++ Count_word.java
@@ -0,0 +1,28 @@
+package inflearn_codingTestPractice;
+
+import java.util.*;
+
+public class Count_word {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String str = input.next().toLowerCase();
+		char ch = input.next().toLowerCase().charAt(0);
+		int count = 0;
+		
+		for (int i = 0; i < str.length(); i++) {
+			if(str.charAt(i) == ch) {
+				count++;
+			}
+		}
+		
+		System.out.println(count);
+	}
+}
+
+/*
+ * [inflearn java algorithm 문제 풀이]
+ * 
+ * 한 개의 문자열을 입력받고, 특정 문자를 입력받아 해당 특정문자가 입력받은 문자열에 몇 개 들어있는지 개수 확인
+ * 대소문자를 구별하지 않는다
+ * 
+ */
