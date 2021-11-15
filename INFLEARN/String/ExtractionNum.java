00eee29463abac91bd192ddef60094b9b1a4c59c
DAHLIACHOI <48826098+DAHLIACHOI@users.noreply.github.com>
2021-11-15 11:45:12 +0900

    extract number

---
+++ INFLEARN/String/ExtractionNum.java
@@ -0,0 +1,20 @@
+package inflearn_codingTestPractice;
+
+
+import java.util.*;
+public class ExtractionNum {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String str = input.next();
+		String answer ="";
+		
+		for(char x : str.toCharArray()) {
+			if( Character.isDigit(x) == true) {
+				answer += x;
+			}
+		}
+		
+		System.out.println(Integer.parseInt(answer));
+	}
+
+}
