17894010f283d46c01cf814f4ec0cf27a81250da
yeji choi <48826098+DAHLIACHOI@users.noreply.github.com>
2024-03-27 21:52:41 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -5,9 +5,5 @@ def solution(brown, yellow):
     for b in range(1, yellow + 1):
         if yellow % b == 0:
             a = yellow // b
-            if a >= b:
-                if (a * 2) + (b * 2) + 4 == brown:
+            if a >= b and (a * 2) + (b * 2) + 4 == brown:
                     return [a + 2, b + 2]
-
-    
-    # return answer
