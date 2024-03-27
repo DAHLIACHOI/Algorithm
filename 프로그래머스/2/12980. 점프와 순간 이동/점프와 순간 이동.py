1918ea413033382d1029524c72ac5c63be0751b3
yeji choi <48826098+DAHLIACHOI@users.noreply.github.com>
2024-03-27 23:23:37 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -1,12 +1,2 @@
 def solution(n):
-    ans = 0
-    
-    while n > 0:
-        # n에서 2를 나눈 나머지가 존재한다면, ans 카운드
-        if n % 2 != 0:
-            ans += 1
-        
-        # n을 절반으로 나누기
-        n = n // 2
-
-    return ans
+    return bin(n).count('1')
