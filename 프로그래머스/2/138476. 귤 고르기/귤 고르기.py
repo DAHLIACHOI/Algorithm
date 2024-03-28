ed20eb84570523a998f8743d8dcb23dfa89ba84d
yeji choi <48826098+DAHLIACHOI@users.noreply.github.com>
2024-03-28 15:15:59 +0900

    [level 2] Title: 귤 고르기, Time: 29.15 ms, Memory: 21.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -0,0 +1,17 @@
+from collections import Counter
+
+def solution(k, tangerine):
+    answer = 0
+    total = 0
+    
+    counter = dict(Counter(tangerine))
+    counter = sorted(counter.items(), key = lambda x : x[1], reverse = True)
+    for x in counter:
+        total += x[1]
+        
+        if total >= k:
+            answer += 1
+            break
+        answer += 1
+    
+    return answer
