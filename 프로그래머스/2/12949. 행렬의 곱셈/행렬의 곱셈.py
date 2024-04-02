20f99be307adbd5b4f8026419ca60d73ece89c6d
yeji choi <48826098+DAHLIACHOI@users.noreply.github.com>
2024-04-02 20:37:11 +0900

    [level 2] Title: 행렬의 곱셈, Time: 6.41 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -0,0 +1,34 @@
+
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+# 문제가 개편되었습니다. 이로 인해 함수 구성이나 테스트케이스가 변경되어, 과거의 코드는 동작하지 않을 수 있습니다.
+# 새로운 함수 구성을 적용하려면 [코드 초기화] 버튼을 누르세요. 단, [코드 초기화] 버튼을 누르면 작성 중인 코드는 사라집니다.
+def solution(A, B):
+    answer = []
+
+    for i in range(len(A)):
+        arr = []
+        for j in range(len(B[0])):
+            tmp = 0
+            for k in range(len(A[0])):
+                tmp += A[i][k] * B[k][j]
+            arr.append(tmp)
+        answer.append(arr)
+
+    return answer
