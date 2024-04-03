69994b39cb1e05b120ea9af9d506c6a791de32c1
yeji choi <48826098+DAHLIACHOI@users.noreply.github.com>
2024-04-03 23:44:54 +0900

    [level 2] Title: 피로도, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,21 +1,3 @@
-answer, n = 0,0
-visited = []
-
-def dfs(k, cnt, dungeons):
-    global answer, visited
-    answer = max(answer, cnt)
-    
-    for j in range(n):
-        if dungeons[j][0] <= k and not visited[j]:
-            visited[j] = 1
-            dfs(k - dungeons[j][1], cnt + 1, dungeons)
-            visited[j] = 0
-    
-
-
 def solution(k, dungeons):
-    global n, visited
-    n = len(dungeons)
-    visited = [0] * n
-    dfs(k, 0, dungeons)
+    answer = -1
     return answer
