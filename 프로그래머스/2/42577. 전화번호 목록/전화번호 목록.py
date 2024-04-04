8e42ef99223cd8521b023063442c4effa542188c
yeji choi <48826098+DAHLIACHOI@users.noreply.github.com>
2024-04-04 18:53:21 +0900

    [level 2] Title: 전화번호 목록, Time: 87.52 ms, Memory: 28 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,17 +1,11 @@
 def solution(phone_book):
     answer = True
     
-    phone_book = sorted(phone_book)
-    tmp = '-1'
+    phone_book.sort()
 
-    for phone in phone_book:
-        n = len(tmp)
-        # 이전꺼랑 접두사가 일치하면
-        if phone[:n] == tmp:
-            return False
-        else:
-            tmp = phone
-            
+    for phone in zip(phone_book, phone_book[1:]):
+            if phone[1].startswith(phone[0]):
+                return False
     
     
     return answer
