1b0cfb85c79038a47c52139e3abd17f040df266f
yeji choi <48826098+DAHLIACHOI@users.noreply.github.com>
2024-04-06 14:14:10 +0900

    [level 2] Title: 더 맵게, Time: 1612.36 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -3,22 +3,20 @@ import heapq
 def solution(scoville, K):
     answer = 0
     
-    h = []
-    for food in scoville:
-        heapq.heappush(h, food)
+    heapq.heapify(scoville)
         
     while True:
         try:
-                    # 첫번째 음식
-            food1 = heapq.heappop(h)
+            # 첫번째 음식
+            food1 = heapq.heappop(scoville)
         
             if food1 >= K:
                 return answer
         
             # 두번째 음식
-            food2 = heapq.heappop(h)
+            food2 = heapq.heappop(scoville)
         
-            heapq.heappush(h, food1 + food2*2)
+            heapq.heappush(scoville, food1 + food2*2)
             answer += 1
         except:
             return -1
