8b99fe2e10357c44db3845cf4385a73c12651a68
yeji choi <48826098+DAHLIACHOI@users.noreply.github.com>
2024-04-17 21:29:57 +0900

    [level 2] Title: 소수 찾기, Time: 0.70 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -0,0 +1,33 @@
+from itertools import permutations
+
+def is_prime_number(x):
+    if x == 2:
+        return True
+    if x == 0 or x == 1:
+        return False
+    
+    for i in range(2, x):
+        if x % i == 0:
+            return False
+    return True
+
+def solution(numbers):
+    
+    answers = set()
+    
+    numbers = list(numbers)
+    answer = 0
+    com = []
+    
+    for i in range(1, len(numbers) + 1):
+        com += list(permutations(numbers, i))
+    
+    for c in com:
+        answers.add(int (''.join(c)))
+    # print(answers)
+        
+    for number in answers:
+        if is_prime_number(number):
+            answer += 1
+    
+    return answer
