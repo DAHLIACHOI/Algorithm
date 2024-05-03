97060f481c5cd8d602abe577396457fc1603914f
yeji choi <48826098+DAHLIACHOI@users.noreply.github.com>
2024-05-03 20:16:19 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.63 ms, Memory: 52.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class Point {
+    int x;
+    int y;
+    
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Solution {
+    
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int[][] dis;
+    
+    public void BFS(int x, int y, int[][] maps) {
+        Queue<Point> Q = new LinkedList<>();
+        dis = new int[maps.length][maps[0].length];
+        
+        Q.offer(new Point(x, y));
+        maps[x][y] = 0;
+        while(!Q.isEmpty()) {
+            Point tmp = Q.poll();
+            for(int i = 0; i < 4; i++) {
+                int nx = tmp.x + dx[i];
+                int ny = tmp.y + dy[i];
+                
+                if (nx >= 0 && nx < maps.length && ny >= 0 && ny < maps[0].length && maps[nx][ny] == 1) {
+                    maps[nx][ny] = 0;
+                    Q.offer(new Point(nx, ny));
+                    dis[nx][ny] = dis[tmp.x][tmp.y] + 1;
+                }
+            }
+        }
+        
+    }
+    
+    public int solution(int[][] maps) {
+        
+        BFS(0, 0, maps);
+        
+        if(dis[maps.length - 1][maps[0].length - 1] == 0) {
+            return -1;
+        }
+        return dis[maps.length - 1][maps[0].length - 1] + 1;
+    }
+}
