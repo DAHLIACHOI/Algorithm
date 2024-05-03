ca63ea70b3ef493081555845d7ef5b574cc11d2c
yeji choi <48826098+DAHLIACHOI@users.noreply.github.com>
2024-05-03 15:16:49 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 45.93 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 같은 숫자는 싫어 - 12906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=java) 
 
 ### 성능 요약
 
-메모리: 27.9 MB, 시간: 41.11 ms
+메모리: 116 MB, 시간: 45.93 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 04일 22:07:55
+2024년 05월 03일 15:16:47
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        Stack<Integer> st = new Stack<>();
+        
+        for(int a : arr) {
+            if (st.isEmpty() || st.peek() != a) st.push(a);
+        }    
+        
+        int[] answer = new int[st.size()];
+        
+        for(int i = 0; i < st.size(); i++) {
+            answer[i] = st.get(i);
+        }
+        
+        return answer;
+    }
+}
